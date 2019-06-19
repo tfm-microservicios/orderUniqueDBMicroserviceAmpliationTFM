4f4a93ede4e28c68436df658f5942bec3081d41f
jliriano <jd.liriano@alumnos.upm.es>
2019-06-19 22:48:34 +0200

    initial work for CloseOrder feature - issue #02

--- src/main/java/es/upm/miw/business_controllers/OrderController.java
+++ src/main/java/es/upm/miw/business_controllers/OrderController.java
@@ -2,7 +2,9 @@ package es.upm.miw.business_controllers;
 
 import es.upm.miw.business_services.RestService;
 import es.upm.miw.documents.Order;
+import es.upm.miw.documents.OrderLine;
 import es.upm.miw.dtos.OrderDto;
+import es.upm.miw.exceptions.BadRequestException;
 import es.upm.miw.repositories.OrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -16,6 +18,7 @@ import java.util.List;
 public class OrderController {
 
     public static final String PROVIDERS_ARTICLES_VALIDATION = "/providers/validate-presence";
+    public static final String PROVIDERS_ARTICLES_UPDATE_STOCK = "/articles/stock-update/{code}/{amount}";
 
     @Autowired
     private OrderRepository orderRepository;
@@ -32,10 +35,29 @@ public class OrderController {
 
     public List<OrderDto> readAll() {
         List<OrderDto> orderDtos = new ArrayList<>();
-        for (Order order : orderRepository.findAll()) {
+        for (Order order : this.orderRepository.findAll()) {
             orderDtos.add(new OrderDto(order));
         }
         return orderDtos;
+    }
+
+    public OrderDto closeOrder(OrderDto orderDto) {
+        String orderId = orderDto.getId();
+        OrderLine[] orderLine = orderDto.getOrderLines();
+        Order closeOrder = this.orderRepository.findById(orderId).orElse(null);
+        if (orderLine.length > 0 && closeOrder != null) {
+            closeOrder.close();
+            closeOrder.setOrderLines(orderLine);
+            closeOrder = this.orderRepository.save(closeOrder);
+        } else {
+            throw new BadRequestException("orderLine is empty");
+        }
+
+        return new OrderDto(closeOrder);
+    }
+
+    public void updateArticleStock(Order closeOrder) {
+        // TO DO -- send PUT requests to Article microservice to update stock amount.
     }
 
 }
--- src/main/java/es/upm/miw/rest_controllers/OrderResource.java
+++ src/main/java/es/upm/miw/rest_controllers/OrderResource.java
@@ -2,12 +2,12 @@ package es.upm.miw.rest_controllers;
 
 import es.upm.miw.business_controllers.OrderController;
 import es.upm.miw.dtos.OrderDto;
+import es.upm.miw.exceptions.BadRequestException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @PreAuthorize("hasRole('ADMIN') or hasRole('MANAGER') or hasRole('OPERATOR')")
@@ -18,6 +18,7 @@ public class OrderResource {
 
     public static final String ORDERS = "/orders";
     public static final String ID = "/{id}";
+    public static final String CLOSE = "/close";
 
     @Autowired
     private OrderController orderController;
@@ -25,6 +26,15 @@ public class OrderResource {
     @GetMapping
     public List<OrderDto> readAll() {
         return this.orderController.readAll();
+    }
+
+    @PostMapping
+    public OrderDto close(@Valid @RequestBody OrderDto orderDto) {
+        if(orderDto.getOrderLines() == null) {
+            throw new BadRequestException("orderLine is empty");
+        } else {
+            return this.orderController.closeOrder(orderDto);
+        }
     }
 
 }
